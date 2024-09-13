62e92877a4150e86d7f24c6ad4154b9efaa95bb8
willianTambori <msn.willian@gmail.com>
2024-09-12 21:32:58 -0300

    Aproveitado a classe banco de dados e seus métodos e implatado no aplicatico ListaVIP

--- AppListaCurso/app/src/main/java/devandroid/tambori/applistacurso/view/SplashActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/tambori/applistacurso/view/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
 
