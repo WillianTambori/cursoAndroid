76ec61de6892d91152852b85fdea34be4ae86268
willianTambori <msn.willian@gmail.com>
2024-09-17 22:04:54 -0300

    habilitado as funções dos botões confirmar digite e mostrar

--- FerramenteLayout2/app/src/main/java/devandroid/tambori/ferramentelayout/MainActivity.java
+++ FerramenteLayout2/app/src/main/java/devandroid/tambori/ferramentelayout/MainActivity.java
@@ -5,11 +5,14 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     TextView txtTitulo;
     EditText editNomeCompleto;
     Button btnConfirmar;
+    ToggleButton tbLigado;
+    SwitchCompat swMostrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         txtTitulo = findViewById(R.id.txtTitulo);
         editNomeCompleto = findViewById(R.id.editNomeCompleto);
         btnConfirmar =findViewById(R.id.btnConfirmar);
+        tbLigado = findViewById(R.id.tbLigado);
+        swMostrar = findViewById(R.id.swMostrar);
 
         btnConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +51,29 @@ public class MainActivity extends AppCompatActivity {
 
                     Toast.makeText(getBaseContext(), "você digitou "+editNomeCompleto.getText(),Toast.LENGTH_LONG).show();
 
+                }
+            }
+        });
+
+        tbLigado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tbLigado.isChecked())
+                    editNomeCompleto.setEnabled(false);
+                else editNomeCompleto.setEnabled(true);
+            }
+        });
+
+        swMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(swMostrar.isChecked())
+                    txtTitulo.setVisibility(View.GONE);
+                else {
+                    txtTitulo.setVisibility(View.VISIBLE);
+                    String novoTitulo = txtTitulo.getText().toString().toUpperCase();
+                    txtTitulo.setText(novoTitulo);
                 }
             }
         });
