61d58d1e04e641ce70fd03f2f571533499b7ca16
willianTambori <msn.willian@gmail.com>
2025-02-25 20:18:49 -0300

    exemplo de Coroutine fora da viewModel

--- Mealz_App/app/src/main/java/devandroid/tambori/mealz_app/ui/meals/MealsCategoriesViewModel.kt
+++ Mealz_App/app/src/main/java/devandroid/tambori/mealz_app/ui/meals/MealsCategoriesViewModel.kt
@@ -3,14 +3,11 @@ package devandroid.tambori.mealz_app.ui.meals
 import androidx.lifecycle.ViewModel
 import devandroid.tambori.mealz_app.model.MealsRepository
 import devandroid.tambori.mealz_app.model.response.MealResponse
-import devandroid.tambori.mealz_app.model.response.MealsCategoriesResponse
 
 class MealsCategoriesViewModel (
     private val repository: MealsRepository = MealsRepository()
 ):ViewModel() {
-    fun getMeals(successCallback: (response: MealsCategoriesResponse?)-> Unit) {
-        return repository.getMeals(){
-            response -> successCallback(response)
-        }
+    suspend fun getMeals(): List<MealResponse> {
+        return repository.getMeals().categories
     }
 }
