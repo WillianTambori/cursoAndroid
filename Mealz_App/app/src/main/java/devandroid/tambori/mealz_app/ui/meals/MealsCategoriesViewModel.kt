a4d3b28454efccc0245c5f98330feb091ed6add7
willianTambori <msn.willian@gmail.com>
2025-02-25 20:55:55 -0300

    exemplo de Coroutine dentro da viewModel, com um scopo personalizado

--- Mealz_App/app/src/main/java/devandroid/tambori/mealz_app/ui/meals/MealsCategoriesViewModel.kt
+++ Mealz_App/app/src/main/java/devandroid/tambori/mealz_app/ui/meals/MealsCategoriesViewModel.kt
@@ -1,13 +1,37 @@
 package devandroid.tambori.mealz_app.ui.meals
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import devandroid.tambori.mealz_app.model.MealsRepository
 import devandroid.tambori.mealz_app.model.response.MealResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MealsCategoriesViewModel (
     private val repository: MealsRepository = MealsRepository()
 ):ViewModel() {
+    private val mealsJob = Job()
+    init {
+        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
+        scope.launch() {
+            val meals = getMeals()
+            mealsState.value = meals
+        }
+
+    }
+
+
+    val mealsState: MutableState<List<MealResponse>> =  mutableStateOf(emptyList<MealResponse>())
+
     suspend fun getMeals(): List<MealResponse> {
         return repository.getMeals().categories
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        mealsJob.cancel()
     }
 }
