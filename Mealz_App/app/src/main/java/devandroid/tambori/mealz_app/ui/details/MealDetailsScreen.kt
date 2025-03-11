f908b014a099484f9921a1004bea442ae946f737
willianTambori <msn.willian@gmail.com>
2025-03-10 21:06:27 -0300

    Utilizando o enum class para modificar a imagem de tamanho e cor das bordas

--- Mealz_App/app/src/main/java/devandroid/tambori/mealz_app/ui/details/MealDetailsScreen.kt
+++ Mealz_App/app/src/main/java/devandroid/tambori/mealz_app/ui/details/MealDetailsScreen.kt
@@ -1,10 +1,15 @@
 package devandroid.tambori.mealz_app.ui.details
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -15,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -28,12 +34,21 @@ import android.service.autofill.Transformation as Transformation1
 @Composable
 
 fun MealDetailsScreen(meal: MealResponse?) {
-    var isExpand: Boolean by remember { mutableStateOf(false)}
-    val imageSizeDp: Dp by animateDpAsState(targetValue = if(isExpand) 200.dp else 100.dp)
+    var profilePictureState by remember { mutableStateOf(MealProfileState.Normal)}
+    val transition = updateTransition(targetState = profilePictureState, label = "")
+    val imageSizeDp by transition.animateDp(targetValueByState = {it.size}, label = "")
+    val color by transition.animateColor(targetValueByState = {it.color}, label = "")
+    val widthSize by transition.animateDp(targetValueByState = {it.borderWidth}, label = "")
 
     Column {
         Row {
-            Card {
+            Card(modifier = Modifier.padding(16.dp),
+                shape = CircleShape,
+                border = BorderStroke(
+                    width = widthSize,
+                    color = color
+                )
+            ) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
                         .data(meal?.imageUrl)
@@ -56,10 +71,24 @@ fun MealDetailsScreen(meal: MealResponse?) {
         Button(
             modifier = Modifier
                 .padding(16.dp),
-            onClick = { isExpand = !isExpand}) {
+            onClick = {
+                profilePictureState = if(profilePictureState == MealProfileState.Normal)
+                    MealProfileState.Expanded
+                else
+                    MealProfileState.Normal
+             }) {
             Text(text = "Change state of meal profile picture")
 
         }
     }
+}
+
+enum class MealProfileState(
+    val color: Color,
+    val size: Dp,
+    val borderWidth: Dp
+){
+    Normal(Color.Magenta,120.dp, 8.dp),
+    Expanded(Color.Green,200.dp, 24.dp)
 }
 
